1451a9212314e71162db443fa0682ef025928bae
sami Um <sammiiiullah@gmail.com>
2024-08-19 23:57:24 +0500

    theme

--- src/context/ThemeIcon.js
+++ src/context/ThemeIcon.js
@@ -1,11 +1,12 @@
 import React , {useContext} from 'react'
 import { ThemeContext } from './GlobalState'
+import { FaLightbulb } from "react-icons/fa";
 
 const ThemeIcon = () => {
     const {  toggleDarkMode , isDarkMode } = useContext(ThemeContext); 
   return (
-    <div>
-      <i className= {`fa-solid fa-lightbulb ${isDarkMode ? 'font-dark' : ''}`} onClick={toggleDarkMode}></i>
+    <div className= {isDarkMode ? 'font-dark' : ''} onClick={toggleDarkMode}>
+      <FaLightbulb />
     </div>
   )
 }
